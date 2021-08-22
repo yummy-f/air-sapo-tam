135a078af9010cb85013e46d947c0bdaf7ee8b55
ryo-1121 <ryo.yamaya@gmail.com>
2021-08-22 23:52:56 +0900

    2021-0822

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-------------------------------- フッター ----------------------------------->
+<footer>
+  <p class="copy_write">&copy; 2021 Three Arrows Management</p>
+</footer>
+
+<?php wp_footer(); ?>
+
+</body>
---
+++ header.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.7/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/sanitize.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/app.css">
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri() ?>/css/custom.css">
+    <link rel="stylesheet" href="https://use.typekit.net/bsy2knc.css">
+
+    <!-- fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Sawarabi+Gothic&display=swap" rel="stylesheet">
+
+    <title>Airsapo TAM</title>
+    
+    <?php wp_head(); ?>
+
+</head>
+
+
+<body>
+    <!------------------------------- ヘッダー -------------------------------------->
+    <header>
+        ヘッダー
+    </header>
