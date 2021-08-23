18cb9e1374426156edcdcb5ebc88abb0bf2c3694
ryo-1121 <ryo.yamaya@gmail.com>
2021-08-24 01:07:41 +0900

    2021-0824

--- footer.php
+++ footer.php
@@ -1,5 +1,22 @@
 <!-------------------------------- フッター ----------------------------------->
 <footer>
+  <div class="footer_link">
+    <img class="footer_logo" src="<?php echo get_template_directory_uri(); ?>/image/logo_black.png" alt="">
+    <ul class="footer_menu1">
+      <li><a href="">HOME</a></li>
+      <li><a href="">事業内容</a></li>
+      <li><a href="">TAM日記</a></li>
+    </ul>
+    <ul class="footer_menu2">
+      <li class="footer_menu2_li1"><a href="">物件のオーナー様へ</a></li>
+      <li><a href="">お問い合わせ</a></li>
+      <li><a href="">プライバシーポリシー</a></li>
+      <li><a href="">会社概要</a></li>
+    </ul>
+  </div>
+
+  <hr>
+
   <p class="copy_write">&copy; 2021 Three Arrows Management</p>
 </footer>
 
--- index.php
+++ index.php
@@ -8,9 +8,55 @@ Template Name: トップページ
 <?php get_header(); ?>
 
 
-<!------------------------------- メイン -------------------------------------->
-<div>
-
+<!------------------------------- FLOW -------------------------------------->
+<div class="flow">
+  <h1>FLOW</h1>
+  <h2>契約までの流れ</h2>
+  <div class="flow_icon_list">
+    <div class="flow_icon">
+      <span class="flow_icon_number">01</span>
+      <div class="icon_box">
+        <p>icon</p>
+      </div>
+      <p class="flow_constract">契約までの流れ</p>
+      <p class="flow_detail">賃料の適正化、募集条件の検証
+        <br>オーナー様へ適正な資料をご提案
+      </p>
+    </div>
+    <div class="flow_icon">
+      <span class="flow_icon_number">02</span>
+      <div class="icon_box">
+        <p>icon</p>
+      </div>
+      <p class="flow_constract">契約までの流れ</p>
+      <p class="flow_detail">賃料の適正化、募集条件の検証
+        <br>オーナー様へ適正な資料をご提案
+      </p>
+    </div>
+    <div class="flow_icon">
+      <span class="flow_icon_number">03</span>
+      <div class="icon_box">
+        <p>icon</p>
+      </div>
+      <p class="flow_constract">契約までの流れ</p>
+      <p class="flow_detail">賃料の適正化、募集条件の検証
+        <br>オーナー様へ適正な資料をご提案
+      </p>
+    </div>
+    <div class="flow_icon">
+      <span class="flow_icon_number">04</span>
+      <div class="icon_box">
+        <p>icon</p>
+      </div>
+      <p class="flow_constract">契約までの流れ</p>
+      <p class="flow_detail">賃料の適正化、募集条件の検証
+        <br>オーナー様へ適正な資料をご提案
+      </p>
+    </div>
+  </div>
+  <div class="more_button">
+    <a href="">MORE</a>
+  </div>
 </div>
 
 
