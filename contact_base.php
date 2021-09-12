36585ae80a212c0891e660709d95682887b91681
ryo-1121 <ryo.yamaya@gmail.com>
2021-09-12 22:11:29 +0900

    2021-0912

---
+++ contact_base.php
@@ -0,0 +1,206 @@
+<div class="contact_subtitle">
+  <h3>物件情報</h3>
+  <p class="notice"><span>*</span> は必須項目となります。必ずご記入ください。</p>
+</div>
+
+<ul class="contact_form">
+  <li>
+    <div class="contact_form_title">ご希望の<br>サービス</div>
+    <div class="contact_form_items form1">[mwform_radio name="service" id="service" children="賃貸募集・賃貸管理,サブリース"]</div>
+  </li>
+  <li>
+    <div class="contact_form_title">物件種別</div>
+    <div class="contact_form_items form1">[mwform_radio name="kinds" children="分譲マンション（区分所有）,一棟アパート・マンション,一戸建て,その他"]</div>
+  </li>
+  <li>
+    <div class="contact_form_title">物件所在地</div>
+    <div class="contact_form_items">[mwform_text name="address" class="input_text" size="60"]</div>
+  </li>
+  <li>
+    <div class="contact_form_title">お部屋の状況</div>
+    <div class="contact_form_items form2">[mwform_radio name="availability" children="空室,入居中,賃貸中,未完成,分からない,その他"]</div>
+  </li>
+  <li>
+    <div class="contact_form_title small_title">現在依頼中の<br>不動産会社</div>
+    <div class="contact_form_items form2 form3">[mwform_radio name="request_company" children="あり,なし"]</div>
+  </li>
+  <li>
+    <div class="contact_form_title">内容<span class="asterisk">*</span></div>
+    <div class="contact_form_items">[mwform_textarea name="content" class="form_content" cols="50" rows="5" placeholder="質問・疑問、細かいご希望等を、ご自由にご入力ください。"]</div>
+  </li>
+</ul>
+
+
+<div class="contact_subtitle customer">
+  <h3>お客様情報</h3>
+</div>
+
+<ul class="contact_form">
+  <li>
+    <div class="contact_form_title">お名前<span class="asterisk">*</span></div>
+    <div class="contact_form_items user_form">[mwform_text class="input_text" name="name" size="60"]</div>
+  </li>
+  <li>
+    <div class="contact_form_title">電話番号<span class="asterisk">*</span></div>
+    <div class="contact_form_items user_form">[mwform_text class="input_text" name="tel_number" size="60"]</div>
+  </li>
+  <li>
+    <div class="contact_form_title">メール<br>アドレス<span class="asterisk">*</span></div>
+    <div class="contact_form_items user_form">[mwform_text class="input_text" name="mail" size="60"]</div>
+  </li>
+</ul>
+
+<div class="check_privacy">
+  <h3>【個人情報のお取扱いについて】</h3>
+  <p>当社がフォームで取得した、お客さまの個人情報は、お客さまへの回答および当社『プライバシーポリシー』に基づいて お取り扱い致します。</p>
+</div>
+
+<div class="check_privacy_content">
+  <div>
+    <h1>第1条（個人情報）</h1>
+    <p>
+      「個人情報」とは，個人情報保護法にいう「個人情報」を指すものとし，生存する個人に関する情報であって，当該情報に含まれる氏名，生年月日，住所，電話番号，連絡先その他の記述等により特定の個人を識別できる情報及び容貌，指紋，声紋にかかるデータ，及び健康保険証の保険者番号などの当該情報単体から特定の個人を識別できる情報（個人識別情報）を指します。
+    </p>
+  </div>
+  <div>
+    <h1>第2条（個人情報の収集方法）</h1>
+
+    <p>
+      当社は，ユーザーが利用登録をする際に氏名，生年月日，住所，電話番号，メールアドレス，銀行口座番号，クレジットカード番号，運転免許証番号などの個人情報をお尋ねすることがあります。また，ユーザーと提携先などとの間でなされたユーザーの個人情報を含む取引記録や決済に関する情報を,当社の提携先（情報提供元，広告主，広告配信先などを含みます。以下，｢提携先｣といいます。）などから収集することがあります。​
+    </p>
+  </div>
+  <div>
+    <h1>第3条（個人情報を収集・利用する目的）</h1>
+
+    <p>
+      当社が個人情報を収集・利用する目的は，以下のとおりです。
+    </p>
+    <ol>
+      <li>当社サービスの提供・運営のため</li>
+      <li>ユーザーからのお問い合わせに回答するため（本人確認を行うことを含む）</li>
+      <li>ユーザーが利用中のサービスの新機能，更新情報，キャンペーン等及び当社が提供する他のサービスの案内のメールを 送付するため</li>
+      <li>メンテナンス，重要なお知らせなど必要に応じたご連絡のため</li>
+      <li>利用規約に違反したユーザーや，不正・不当な目的でサービスを利用しようとするユーザーの特定をし，ご利用をお断 りするため</li>
+      <li>ユーザーにご自身の登録情報の閲覧や変更，削除，ご利用状況の閲覧を行っていただくため</li>
+      <li>有料サービスにおいて，ユーザーに利用料金を請求するため</li>
+      <li>上記の利用目的に付随する目的</li>
+    </ol>
+  </div>
+  <div>
+    <h1>第4条（利用目的の変更）</h1>
+
+    <ol>
+      <li>当社は，利用目的が変更前と関連性を有すると合理的に認められる場合に限り，個人情報の利用目的を変更するものとします。</li>
+      <li>利用目的の変更を行った場合には，変更後の目的について，当社所定の方法により，ユーザーに通知し，または本ウェ ブサイト上に公表するものとします。</li>
+    </ol>
+  </div>
+  <div>
+    <h1>第5条（個人情報の第三者提供）</h1>
+
+    <ol>
+      <li>
+        当社は，次に掲げる場合を除いて，あらかじめユーザーの同意を得ることなく，第三者に個人情報を提供することはあ りません。ただし，個人情報保護法その他の法令で認められる場合を除きます。
+        <ul>
+          <li>人の生命，身体または財産の保護のために必要がある場合であって，本人の同意を得ることが困難であるとき</li>
+          <li>公衆衛生の向上または児童の健全な育成の推進のために特に必要がある場合であって，本人の同意を得ることが困難であるとき</li>
+          <li>国の機関もしくは地方公共団体またはその委託を受けた者が法令の定める事務を遂行することに対して協力する必要 がある場合であって，本人の同意を得ることにより当該事務の遂行に支障を及ぼすおそれがあるとき
+          </li>
+          <li>予め次の事項を告知あるいは公表し，かつ当社が個人情報保護委員会に届出をしたとき</li>
+        </ul>
+        <ul class="org_style">
+          <li>利用目的に第三者への提供を含むこと</li>
+          <li>第三者に提供されるデータの項目</li>
+          <li>第三者への提供の手段または方法</li>
+          <li>本人の求めに応じて個人情報の第三者への提供を停止すること</li>
+          <li>本人の求めを受け付ける方法</li>
+        </ul>
+      </li>
+      <li>
+        前項の定めにかかわらず，次に掲げる場合には，当該情報の提供先は第三者に該当しないものとします。
+        <ul class="org_style">
+          <li>当社が利用目的の達成に必要な範囲内において個人情報の取扱いの全部または一部を委託する場合</li>
+          <li>合併その他の事由による事業の承継に伴って個人情報が提供される場合</li>
+          <li>
+            個人情報を特定の者との間で共同して利用する場合であって，その旨並びに共同して利用される個人情報の項目，共同して利用する者の範囲，利用する者の利用目的および当該個人情報の管理について責任を有する者の氏名または名称について，あらかじめ本人に通知し，または本人が容易に知り得る状態に置いた場合
+          </li>
+        </ul>
+      </li>
+    </ol>
+  </div>
+  <div>
+    <h1>第6条（個人情報の開示）</h1>
+
+    <ol>
+      <li>
+        当社は，本人から個人情報の開示を求められたときは，本人に対し，遅滞なくこれを開示します。ただし，開示することにより次のいずれかに該当する場合は，その全部または一部を開示しないこともあり，開示しない決定をした場合には，その旨を遅滞なく通知します。なお，個人情報の開示に際しては，1件あたり1，000円の手数料を申し受けます。
+        <ul class="org_style">
+          <li>本人または第三者の生命，身体，財産その他の権利利益を害するおそれがある場合</li>
+          <li>当社の業務の適正な実施に著しい支障を及ぼすおそれがある場合</li>
+          <li>その他法令に違反することとなる場合</li>
+        </ul>
+      </li>
+      <li>
+        前項の定めにかかわらず，履歴情報および特性情報などの個人情報以外の情報については，原則として開示いたしません。
+      </li>
+    </ol>
+  </div>
+  <div>
+    <h1>第7条（個人情報の訂正および削除）</h1>
+
+    <ol>
+      <li>ユーザーは，当社の保有する自己の個人情報が誤った情報である場合には，当社が定める手続きにより，当社に対して個人情報の訂正，追加または削除（以下，「訂正等」といいます。）を請求することができます。
+      </li>
+      <li>当社は，ユーザーから前項の請求を受けてその請求に応じる必要があると判断した場合には，遅滞なく，当該個人情報 の訂正等を行うものとします。</li>
+      <li>当社は，前項の規定に基づき訂正等を行った場合，または訂正等を行わない旨の決定をしたときは遅滞なく，これをユーザーに通知します。</li>
+    </ol>
+  </div>
+  <div>
+    <h1>第8条（個人情報の利用停止等）</h1>
+
+    <ol>
+      <li>
+        当社は，本人から，個人情報が，利用目的の範囲を超えて取り扱われているという理由，または不正の手段により取得されたものであるという理由により，その利用の停止または消去（以下，「利用停止等」といいます。）を求められた場合には，遅滞なく必要な調査を行います。
+      </li>
+      <li>前項の調査結果に基づき，その請求に応じる必要があると判断した場合には，遅滞なく，当該個人情報の利用停止等を行います。</li>
+      <li>当社は，前項の規定に基づき利用停止等を行った場合，または利用停止等を行わない旨の決定をしたときは，遅滞なく，これをユーザーに通知します。</li>
+      <li>前2項にかかわらず，利用停止等に多額の費用を有する場合その他利用停止等を行うことが困難な場合であって，ユー
+        ザーの権利利益を保護するために必要なこれに代わるべき措置をとれる場合は，この代替策を講じるものとします。
+      </li>
+    </ol>
+  </div>
+  <div>
+    <h1>第9条（プライバシーポリシーの変更）</h1>
+
+    <ol>
+      <li>本ポリシーの内容は，法令その他本ポリシーに別段の定めのある事項を除いて，ユーザーに通知することなく，変更することができるものとします。</li>
+      <li>当社が別途定める場合を除いて，変更後のプライバシーポリシーは，本ウェブサイトに掲載したときから効力を生じるものとします。</li>
+    </ol>
+  </div>
+  <div>
+    <h1>第10条（お問い合わせ窓口）</h1>
+
+    <p>
+      本ポリシーに関するお問い合わせは，下記の窓口までお願いいたします。<br>
+      住所：東京都港区芝4-3-11 VORT三田駅前3F<br>
+      社名：株式会社スリーアローズ<br>
+      Eメールアドレス：info@ta-japan.com
+    </p>
+  <p class="mb-5">以上</p>
+  </div>
+</div>
+
+<div class="privacy_check">
+  <input type="checkbox" id="scales" name="scales">
+  <label for="scales">プライバシーポリシーに同意する</label>
+</div>
+
+<p class="privacy_check_p">SSL対応　当ウェブサイトでは、プライバシー保護のため、データを暗号化して送受信します。</p>
+
+<div class="check_btn_div">
+[mwform_bconfirm class="check_btn" value="confirm"]送信内容確認へ[/mwform_bconfirm]
+
+
+[mwform_bback class="confirm_btn check_btn" value="back"]戻る[/mwform_bback]
+
+[mwform_bsubmit name="submit" class="confirm check_btn" value="send"]送信する[/mwform_bsubmit]
+</div>
