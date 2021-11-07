a13fc1c6e4a48c41212ba5447b409cb2a91b0429
ryo-1121 <ryo.yamaya@gmail.com>
2021-11-07 17:35:36 +0900

    2021-1107

---
+++ page-interview.php
@@ -0,0 +1,52 @@
+<?php
+/*
+Template Name: インタビュー
+*/
+?>
+
+
+<?php get_header(); ?>
+
+
+<!------------------------------- インタビュー -------------------------------------->
+<div class="background_other">
+  <h1>INTERVIEW</h1>
+  <h2>インタビュー</h2>
+</div>
+
+<div class="subtitle">
+  <p>HOME インタビュー　平木真由子</p>
+</div>
+
+<div class="interview_container">
+  <!------------------------------- インタビューTOP -------------------------------------->
+
+  <section class="interview_top">
+    <div class="interview_top_box">
+      <div class="interview_top_box_img">
+        <img src="<?php echo get_template_directory_uri(); ?>/image/interview01.png" alt="">
+      </div>
+      <div class="interview_top_box_introduction">
+        <p>会社紹介インタビュー</p>
+        <h2>平木 真由子 <span>Mayuko Hiraki</span></h2>
+        <h3>常に時代の変化を感じて<br>新たな不動産活用を</h3>
+      </div>
+    </div>
+  </section>
+
+  <!------------------------------- インタビュー01 -------------------------------------->
+  <section class="interview_01">
+    <div class="interview_01_cont">
+      <div class="interview_01_cont_title">
+      <p class="interview_01_cont_title_num">01</p><p class="interview_01_cont_title_text">スリーアローズとはどんな会社ですか？</p>
+      </div>
+
+    </div>
+  </section>
+
+
+
+
+
+</div>
+<?php get_footer(); ?>
