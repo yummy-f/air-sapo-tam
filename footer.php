cd05ee1d20a3a05727817d4dc5dbb4f71c5c9a1a
ryo-1121 <ryo.yamaya@gmail.com>
2021-11-20 18:27:39 +0900

    wip

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-------------------------------- フッター ----------------------------------->
 <footer>
-  <a href="" class="top_back_arrow"><img src="<?php echo get_template_directory_uri(); ?>/image/up_arrow.png" alt=""></a>
+  <a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="top_back_arrow"><img src="<?php echo get_template_directory_uri(); ?>/image/up_arrow.png" alt=""></a>
   <div class="footer_link">
     <div>
       <a href="/"><img class="footer_logo" src="<?php echo get_template_directory_uri(); ?>/image/logo_red.png" alt=""></a>
